30cf4714d2bb408e03614e250e095d8d0a79ce04
sophiabrothers1 <107134583+sophiabrothers1@users.noreply.github.com>
2024-09-23 15:49:45 -0400

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -4,38 +4,31 @@
 # Date: 23 September 2024
 # Contact: sophia.brothers@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: opendatatoronto, tidyverse, readxl
+# Pre-requisites: opendatatoronto, tidyverse, dplyr
 # Any other information needed? N/A
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-library(readxl)
-# [...UPDATE THIS...]
+library(dplyr)
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(tidyverse)
+library(dplyr)
+
 package <- show_package("97d72233-8681-4b3d-828e-4929d40d122c")
+resources <- list_package_resources(package)
+
+resource_2 <- resources[2, ]
+the_raw_data <- get_resource(resource_2)
 
 #### Download data ####
 # [...ADD CODE HERE TO DOWNLOAD...]
-resources <- package$resources
-all_data <- data.frame()
-for (i in 1:25) {
-  ward_name <- paste0("Ward ", i)
-  resource <- resources[grepl(ward_name, resources$name), ]
-  
-  if (nrow(resource) > 0) {
-    temp_file <- tempfile(fileext = "2023 Office of the Mayor.xlsx")
-    download.file(resource$url, temp_file, mode = "wb")
-    
-    ward_data <- read_excel(temp_file, sheet = ward_name) %>%
-      mutate(Ward = ward_name)
-    
-    all_data <- bind_rows(all_data, ward_data)
-    
-    unlink(temp_file)
+for (i in seq_along(the_raw_data)) {
+  if (is.data.frame(the_raw_data[[i]])) {
+    write_csv(the_raw_data[[i]], paste0("raw_data", i, ".csv"))
   }
 }
 
-#### Save data ####
-# [...UPDATE THIS...]
-write_csv(all_data, "data/raw_data/2023_mayoral_election_results.csv")
