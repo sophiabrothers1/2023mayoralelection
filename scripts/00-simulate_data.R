13a9937dc35fb72d219f414c4fac526341ab1d52
sophiabrothers1 <107134583+sophiabrothers1@users.noreply.github.com>
2024-09-26 22:37:34 -0400

    Update 00-simulate_data.R to include a simulated electoral dataset with tests

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -4,18 +4,16 @@
 # Date: 23 September 2024
 # Contact: sophia.brothers@mail.utoronto.ca
 # License: MIT CHECK THIS
-# Pre-requisites: tidyverse
-# Any other information needed? No
-
+# Pre-requisites: tidyverse, dplyr, ggplot2
+# Any other information needed? This simulation uses fictional data.
 
 #### Workspace setup ####
-# [...UPDATE THIS...]
 library(tidyverse)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
 set.seed(123)
 
+# set up the variables
 num_wards <- 25
 num_subdivisions_per_ward <- 99
 candidates <- c("Abdulsalam Bahira",
@@ -123,10 +121,50 @@ candidates <- c("Abdulsalam Bahira",
 )
 subdivisions <- paste0("Subdivision ", 1:99)
 
+# expand the dataset so there are 99 subdivsions and all of the candidates in each ward
 simulated_data <- expand_grid(
   ward = paste0("Ward ", 1:num_wards),
   subdivision = paste0("Subdivision ", 1:num_subdivisions_per_ward),
   candidate = candidates
 ) %>%
   
-  mutate(votes = sample(10:500, size = n(), replace = TRUE))
+# randomly assign votes to different candidates in different subdivisions and wards
+  mutate(votes = sample(0:20000, size = n(), replace = TRUE))
+
+
+# tests
+
+# test 1: 25 wards exist
+unique_wards <- length(unique(simulated_data$ward))
+expected_wards <- num_wards
+stopifnot(unique_wards == expected_wards)
+cat("Test 1 Passed\n")
+
+# test 2: each ward has 99 subdivisions
+ward_subdivision_counts <- simulated_data %>%
+  group_by(ward) %>%
+  summarise(subdivisions_count = n_distinct(subdivision))
+
+stopifnot(all(ward_subdivision_counts$subdivisions_count == num_subdivisions_per_ward))
+cat("Test 2 Passed\n")
+
+# test 3: each ward/subdivision has all candidates
+ward_subdivision_candidate_counts <- simulated_data %>%
+  group_by(ward, subdivision) %>%
+  summarise(candidates_count = n_distinct(candidate))
+
+expected_candidates <- length(candidates)
+stopifnot(all(ward_subdivision_candidate_counts$candidates_count == expected_candidates))
+cat("Test 3 Passed\n")
+
+# 4. test 4: randomized votes are within 10 and 500
+min_votes <- min(simulated_data$votes)
+max_votes <- max(simulated_data$votes)
+stopifnot(min_votes >= 0 & max_votes <= 20000)
+cat("Test 4 Passed\n")
+
+# 5. test 5: expected # of rows in dataset
+expected_rows <- num_wards * num_subdivisions_per_ward * length(candidates)
+actual_rows <- nrow(simulated_data)
+stopifnot(expected_rows == actual_rows)
+cat("Test 5 Passed\n")
