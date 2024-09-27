4da41925693ee1e3520e0d6a802523b7a3d3ac64
sophiabrothers1 <107134583+sophiabrothers1@users.noreply.github.com>
2024-09-26 22:38:19 -0400

    Update 01-download_data.R to show how the dataset was downloaded

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -13,22 +13,20 @@ library(opendatatoronto)
 library(tidyverse)
 library(dplyr)
 
-#### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
-library(dplyr)
-
+# get package from opendatatoronto
 package <- show_package("97d72233-8681-4b3d-828e-4929d40d122c")
 resources <- list_package_resources(package)
 
+# select the 2023 mayoral byelection specifically from the package
 resource_2 <- resources[2, ]
 the_raw_data <- get_resource(resource_2)
 
+
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+
+# convert into a CSV
 for (i in seq_along(the_raw_data)) {
   if (is.data.frame(the_raw_data[[i]])) {
     write_csv(the_raw_data[[i]], paste0("raw_data", i, ".csv"))
   }
 }
-
